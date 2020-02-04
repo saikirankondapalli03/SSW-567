0e30a39729a1957864285d134a008dc6a08f44f5
Sai Kiran Kondapalli <saikirankondapalli03@gmail.com>
2020-02-04 18:22:13 -0500

    Update TestTriangle.py

--- HW02-a/TestTriangle.py
+++ HW02-a/TestTriangle.py
@@ -10,7 +10,7 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 import unittest
 import HtmlTestRunner
 
-from Triangle import classifyTriangle
+from ./Triangle import classifyTriangle
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
